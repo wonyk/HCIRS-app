dc6fa11107617ae2d0b87190f2728774224e5844
wonyk <yingkeatwon@gmail.com>
2017-04-21 14:09:25 +0000

    Fixed Login
    Working on state changes next

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,12 +1,31 @@
-(function () {
+(function() {
     angular
         .module('hcgc-app')
         .controller('settingsCtrl', settingsCtrl);
 
-    settingsCtrl.$inject = ['$scope'];
+    settingsCtrl.$inject = ['$scope', '$firebaseAuth'];
 
-    function settingsCtrl($scope) {
+    function settingsCtrl($scope, $firebaseAuth) {
+        $scope.authObj = $firebaseAuth();
+        $scope.user = {};
+        $scope.login = function() {
 
-    };
+            $scope.authObj.$signInWithEmailAndPassword($scope.user.email, $scope.user.password)
+                .then(function(firebaseUser) {
+                    console.log("Signed in as:", firebaseUser.uid);
+                }).catch(function(error) {
+                    console.error("Authentication failed:", error);
+                });
+        };
+        /*
+        $scope.authObj.$createUserWithEmailAndPassword("my@email.com", "mypassword")
+            .then(function(firebaseUser) {
+                console.log("User " + firebaseUser.uid + " created successfully!");
+            }).catch(function(error) {
+                console.error("Error: ", error);
+            });
+            */
+    }
+
 
 })();
