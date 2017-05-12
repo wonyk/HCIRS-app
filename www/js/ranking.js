b7cd64ee3a1a6f7eeda83edacd94abe1b9d7aeb3
wonyk <yingkeatwon@gmail.com>
2017-05-12 22:47:45 +0800

    COMPLETED Working Leaderboard!!!

--- www/js/ranking.js
+++ www/js/ranking.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     angular
         .module('hcirs-app')
         .controller('rankCtrl', rankCtrl);
@@ -6,16 +6,28 @@
     rankCtrl.$inject = ['$scope', '$state', '$ionicLoading', '$firebaseObject'];
 
     function rankCtrl($scope, $state, $ionicLoading, $firebaseObject) {
-        var ref = firebase.database().ref();
-
-        var obj = $firebaseObject(ref);
-
-        // to take an action after the data loads, use the $loaded() promise
-        obj.$loaded().then(function() {
-            console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-
+        $ionicLoading.show({
+            template: '<p>Loading...</p><ion-spinner></ion-spinner>'
         });
+        var ref = firebase.database().ref();
+        var players = $firebaseObject(ref);
 
+        var getPlayerScore = function () {
+            $scope.players = [];
+            players.$loaded().then(function () {
+                angular.forEach(players, function (value, key) {
+                    $scope.players.push(value.details);
+                });
+                $scope.$broadcast('scroll.refreshComplete');
+                $ionicLoading.hide();
+            });
+        };
+
+        getPlayerScore();
+
+        $scope.refresh = function () {
+            getPlayerScore();
+        };
     }
 
 
